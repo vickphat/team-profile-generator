b28c202cc0d152a32c12ccade813cfd10ed1a8ba
Vicheka Phat <vickphat@gmail.com>
2021-05-02 23:50:18 -0400

    added new line of code to engineer.test.js

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -4,12 +4,13 @@ describe("Engineer", () => {
 
     it("Should create an engineer object", () => {
 
-        const test = new engineer("Vick", "3", "vickphat@gmail.com");
+        const test = new engineer("Vick", "3", "vickphat@gmail.com", "github" );
 
         expect(test.name).toBe("Vick");
         expect(test.id).toBe("3");
         expect(test.email).toBe("vickphat@gmail.com");
         expect(test.title).toBe("Engineer");
+        expect(test.github).toBe("github");
     });
 
     it("Should obtain role via getRole()", () => {
